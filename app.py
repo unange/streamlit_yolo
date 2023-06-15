e1f4d1f1670161a78e1e5770d35540902ec6f66a
unange <136736709+unange@users.noreply.github.com>
2023-06-16 00:32:33 +0700

    app.py

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ import numpy as np
 import torch, json , cv2 , detect
 
 
-st.title("🌊 Under the sea detection")
+st.title("humen")
 
 st.write("Upload your Image...")
 
 #model = torch.hub.load('./yolov5', 'custom', path='./last.pt', source='local')
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/last.pt', force_reload=True)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/best.pt', force_reload=True)
 
 uploaded_file = st.file_uploader("Choose .jpg pic ...", type="jpg")
 if uploaded_file is not None:
