ea48fae4d310361f4d7cf84e32845240d7d8077e
unange <136736709+unange@users.noreply.github.com>
2023-06-16 01:20:16 +0700

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.title("humen")
 st.write("Upload your Image...")
 
 #model = torch.hub.load('./yolov5', 'custom', path='./last.pt', source='local')
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/best.pt', force_reload=True)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/best(1).pt', force_reload=True)
 
 uploaded_file = st.file_uploader("Choose .jpg pic ...", type="jpg")
 if uploaded_file is not None:
